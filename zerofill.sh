1f39c23236b8f901b942e127ba1fc7e9dde0dde8
igorscherer <64175650+igorscherer@users.noreply.github.com>
2023-09-18 12:18:39 -0300

    Update zerofill.sh

--- zerofill.sh
+++ zerofill.sh
@@ -1,47 +1,54 @@
 #!/bin/bash
-#Script para ZeroFill com ate 4 HDDs simultaneos
-#Criador Igor Scherer
-#Criado em 01/09/2022
-#Atualizado em 31/05/2023 (Adicionado função para registar serial number dos hdds)
 
-remotefilelog=/IMAGENS/seriais.txt
+# Script para ZeroFill com até 4 HDDs simultâneos
+# Criador: Igor Scherer
+# Criado em: 01/09/2022
+# Atualizado em: 31/05/2023 (Adicionada função para registrar o serial number dos HDDs)
 
-getsnhds(){
-	{ echo -n "Serial Computador: "; dmidecode -s system-serial-number; date +"%d-%m-%y %T"; } >> $remotefilelog;
-	{ echo "Seriais dos HDs "; lshw -class disk | grep "serial:"; }  >> $remotefilelog;
-	echo -e "\n" >> $remotefilelog;
-	echo "Seriais enviados para $remotefilelog";
+# Arquivo de log remoto
+remotefilelog="/IMAGENS/seriais.txt"
+
+# Função para obter o serial number do computador e dos HDDs
+get_sn_hds() {
+    {
+        echo -n "Serial Computador: "
+        dmidecode -s system-serial-number
+        date +"%d-%m-%y %T"
+    } >> "$remotefilelog"
+
+    {
+        echo "Seriais dos HDs"
+        lshw -class disk | grep "serial:"
+    } >> "$remotefilelog"
+
+    echo -e "\n" >> "$remotefilelog"
+    echo "Seriais enviados para $remotefilelog"
 }
 
-zerahd1(){
-	dd if=/dev/zero of=/dev/sda bs=4M status=progress;
-	echo "HDD 1 Zerado com sucesso";
-}
-zerahd2(){
-	dd if=/dev/zero of=/dev/sdb bs=4M status=progress;
-	echo "HDD 2 Zerado com sucesso";
-}
-zerahd3(){
-	dd if=/dev/zero of=/dev/sdc bs=4M status=progress;
-	echo "HDD 3 Zerado com sucesso";
-}
-zerahd4(){
-	dd if=/dev/zero of=/dev/sdd bs=4M status=progress;
-	echo "HDD 4 Zerado com sucesso";
+# Função para zerar um disco
+zerar_hd() {
+    local disco="$1"
+    dd if=/dev/zero of="$disco" bs=4M status=progress
+    echo "Disco $disco zerado com sucesso"
 }
 
-zeratodos(){
-getsnhds;
-zerahd1 &
-zerahd2 &
-zerahd3 &
-zerahd4 &
+# Função para zerar todos os discos
+zerar_todos() {
+    get_sn_hds
 
-wait
+    # Lista de discos a serem zerados
+    discos=("/dev/sda" "/dev/sdb" "/dev/sdc" "/dev/sdd")
 
-echo -e "\a"; sleep .5; echo -e "\a"; sleep .5; echo -e "\a";
-echo "Todos Hds foram zerados, desligando o computador em 1 minuto.";
-shutdown -P +1
+    for disco in "${discos[@]}"; do
+        zerar_hd "$disco" &
+    done
 
+    wait
+
+    echo -e "\a"; sleep 0.5; echo -e "\a"; sleep 0.5; echo -e "\a"
+    echo "Todos os HDDs foram zerados, desligando o computador em 1 minuto."
+    shutdown -P +1
 }
-zeratodos
+
+# Chama a função para zerar todos os discos
+zerar_todos
