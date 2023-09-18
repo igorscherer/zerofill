ad241e2398dcbdae70f6ff38daa6204cdc8ee5fe
igorscherer <64175650+igorscherer@users.noreply.github.com>
2023-09-18 12:15:28 -0300

    Update zerofill.sh

--- zerofill.sh
+++ zerofill.sh
@@ -1,27 +1,37 @@
+#!/bin/bash
 #Script para ZeroFill com ate 4 HDDs simultaneos
+#Criador Igor Scherer
+#Criado em 01/09/2022
+#Atualizado em 31/05/2023 (Adicionado função para registar serial number dos hdds)
 
-#Funcao de zerar o Hd1
+remotefilelog=/IMAGENS/seriais.txt
+
+getsnhds(){
+	{ echo -n "Serial Computador: "; dmidecode -s system-serial-number; date +"%d-%m-%y %T"; } >> $remotefilelog;
+	{ echo "Seriais dos HDs "; lshw -class disk | grep "serial:"; }  >> $remotefilelog;
+	echo -e "\n" >> $remotefilelog;
+	echo "Seriais enviados para $remotefilelog";
+}
+
 zerahd1(){
 	dd if=/dev/zero of=/dev/sda bs=4M status=progress;
-	echo HDD 1 Zerado com sucesso;
+	echo "HDD 1 Zerado com sucesso";
 }
-#Funcao de zerar o Hd2
 zerahd2(){
 	dd if=/dev/zero of=/dev/sdb bs=4M status=progress;
-	echo HDD 2 Zerado com sucesso;
+	echo "HDD 2 Zerado com sucesso";
 }
-#Funcao de zerar o Hd3
 zerahd3(){
 	dd if=/dev/zero of=/dev/sdc bs=4M status=progress;
-	echo HDD 3 Zerado com sucesso;
+	echo "HDD 3 Zerado com sucesso";
 }
-#Funcao de zerar o Hd4
 zerahd4(){
 	dd if=/dev/zero of=/dev/sdd bs=4M status=progress;
-	echo HDD 4 Zerado com sucesso;
+	echo "HDD 4 Zerado com sucesso";
 }
-#Funcao de zerar todos hdds
+
 zeratodos(){
+getsnhds;
 zerahd1 &
 zerahd2 &
 zerahd3 &
@@ -30,9 +40,8 @@ zerahd4 &
 wait
 
 echo -e "\a"; sleep .5; echo -e "\a"; sleep .5; echo -e "\a";
-echo Todos Hds foram zerados, desligando o computador em 1 minuto.;
+echo "Todos Hds foram zerados, desligando o computador em 1 minuto.";
 shutdown -P +1
 
 }
-#Chama a funcao para zerar todos hdds
 zeratodos
